7fb44bac6b976ad519db4d577751206eb841bf71
jsantigs <jsantigs@gmail.com>
2022-03-06 23:21:00 +0100

    Pre-deployment Vercel

---
+++ pages/_app.tsx
@@ -0,0 +1,24 @@
+import { ReactElement, ReactNode } from 'react';
+
+import { NextPage } from 'next';
+import { AppProps } from 'next/app';
+
+import '../styles/globals.css'
+
+type NexPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement ) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NexPageWithLayout
+}
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+
+  const getLayout = Component.getLayout || ((page) => page) ;
+
+  // return <Component {...pageProps} />
+  return getLayout(<Component {...pageProps} />);
+}
+
+export default MyApp
---
+++ pages/about.tsx
@@ -0,0 +1,35 @@
+import { ReactElement } from 'react';
+import Link from 'next/link'
+import { DarkLayout } from '../components/layouts/DarkLayout'
+import { MainLayout } from '../components/layouts/MainLayout'
+
+export default function AboutPage() {
+  return (
+
+    <>
+      <h1>About page</h1>
+
+      <h1 className={'title'}>
+        Ir a <Link href="/">Home</Link>
+      </h1>
+
+      <p className={'description'}>
+        Get started by editing{' '}
+        <code className={'code'}>pages/about.jsx</code>
+      </p>
+    </>
+  )
+}
+
+AboutPage.getLayout = function getLayout( page: ReactElement ) {
+  return (
+    <MainLayout>
+      <DarkLayout>
+        { page }
+      </DarkLayout>
+    </MainLayout>
+  )
+  
+} 
+
+
